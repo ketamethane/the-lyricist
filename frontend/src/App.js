a2815f817af8fa2eb1444ea9fb7e0cdb63fd0466
ketamethane <cheonghsien@gmail.com>
2024-08-20 20:38:25 +0800

    Rename frontend directory name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { LyricsPage } from './components/LyricsPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <LyricsPage/>
+    </div>
+  );
+}
+
+export default App
+
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +25,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
